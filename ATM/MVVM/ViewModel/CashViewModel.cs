2bf3553eb984726fc4420868f0950f20fb15bec5
Artem <a.v.lemyakin@gmail.com>
2023-08-21 12:12:58 +0700

    Убрал бизнес-логику Cash в отдельный скрипт

--- ATM/MVVM/ViewModel/CashViewModel.cs
+++ ATM/MVVM/ViewModel/CashViewModel.cs
@@ -1,7 +1,4 @@
 ﻿using ATM.Core;
-using ATM.MVVM.Model;
-using System.Collections.ObjectModel;
-using System.Linq;
 using System;
 
 namespace ATM.MVVM.ViewModel
@@ -17,7 +14,7 @@ namespace ATM.MVVM.ViewModel
         public int SelectedDenomination { get => selectedDenomination; set { selectedDenomination = value; OnPropertyChanged(nameof(SelectedDenomination)); } }
 
         private int desireSumm = 0;
-        public int DesireSumm { get => desireSumm; set { if (NumberValidationTextBox(value.ToString())) desireSumm = value; else desireSumm = 0; OnPropertyChanged(nameof(DesireSumm)); } }        
+        public int DesireSumm { get => desireSumm; set { if (NumberValidationTextBox(value.ToString()) && value > 0) desireSumm = value; else desireSumm = 0; OnPropertyChanged(nameof(DesireSumm)); } }        
 
         public CashViewModel()
         {
@@ -39,65 +36,19 @@ namespace ATM.MVVM.ViewModel
 
         private void TryCash()
         {
-            if (desireSumm > 0)
+            for (int i = 0; i < MainVM.MoneyCassettes.Count; i++)
             {
-                for (int i = 0; i < MainVM.MoneyCassettes.Count; i++)
+                if (desireSumm % MainVM.MoneyCassettes[i].Denomination == 0)
                 {
-                    if (desireSumm % MainVM.MoneyCassettes[i].Denomination == 0)
-                    {
-                        MainVM.Message = Cash(desireSumm, selectedDenomination, MainVM.MoneyCassettes);
-                        OnPropertyChanged(nameof(MainVM.TotalSum));
-                        return;
-                    }
+                    MainVM.Message = CashService.Cash(desireSumm, selectedDenomination, MainVM.MoneyCassettes);
+                    OnPropertyChanged(nameof(MainVM.TotalSum));
+                    return;
                 }
             }
             DesireSumm = 0;
             MainVM.Message.Text = "Non correct input. Please enter another amount.";
             MainVM.Message.Color = "#FF0000";                        
-        }
-
-        public MessageModel Cash(int desireSumm, int desireBancnote, ObservableCollection<MoneyCassetteModel> moneyCassette)
-        {
-            if (moneyCassette.Sum(item => item.CountBill) > 0)
-            {
-                MoneyCassetteModel cassette = new MoneyCassetteModel();
-                for (int i = moneyCassette.Count - 1; i >= 0; i--)
-                    if (moneyCassette[i].Denomination == desireBancnote)
-                        cassette = moneyCassette[i];
-
-                if (desireSumm >= desireBancnote && cassette.CountBill > 0)
-                {
-                    cassette.CountBill -= desireSumm / desireBancnote;
-                    if (cassette.CountBill < desireSumm / desireBancnote)
-                        desireSumm = (desireSumm / desireBancnote - cassette.CountBill) * desireBancnote;
-                    else
-                        desireSumm -= desireSumm / desireBancnote * desireBancnote; //Находим остаток желаемой суммы с помощью div
-                }
-                if (desireSumm > 0 || cassette.CountBill == 0)
-                {
-                    desireBancnote = GetMatchingCassette(desireSumm, cassette, moneyCassette).Denomination;
-                    Cash(desireSumm, desireBancnote, moneyCassette);
-                }
-            }
-            else
-            {
-                return new MessageModel() { Text = "Sorry, no matching banknotes ;(", Color = "#FF0000" };
-            }
-            return new MessageModel() { Text = "Operation successful!", Color = "#008000" };
-        }
-
-        private MoneyCassetteModel GetMatchingCassette(int balance, MoneyCassetteModel cassette, ObservableCollection<MoneyCassetteModel> moneyCassette)
-        {
-            for (int i = moneyCassette.Count - 1; i >= 0; i--)
-            {
-                if (moneyCassette[i].CountBill > 0 && balance >= moneyCassette[i].Denomination)
-                {
-                    cassette = moneyCassette[i];
-                    return cassette;
-                }
-            }
-            return cassette;
-        }
+        }        
 
         public void SetDefault()
         {
